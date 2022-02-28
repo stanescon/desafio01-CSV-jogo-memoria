8e84f3b551f1924850d1e9f54f53a9c7d922286e
stanescon <stanescon.n@gmail.com>
2022-02-28 17:23:07 -0300

    ajustando multiplayers

--- script.js
+++ script.js
@@ -6,6 +6,40 @@ let contadorChange = 1
 let imagemAberta = ""
 let imagemAnterior = ""
 let arr = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17,18,18]
+const listaNumeroDeJogadores = document.querySelectorAll('.jogadores');
+let numeroDeJogadores = ""
+let contadorMultiplayer = 0
+let pontosPlayer1 = 0
+let pontosPlayer2 = 0
+let pontosPlayer3 = 0
+let pontosPlayer4 = 0
+
+
+function jogadorAtivo (contadorMultiplayer) {
+    for(let i=0; i < numeroDeJogadores ; i++){
+        if(contadorMultiplayer  == i){
+            for(j=0; j < numeroDeJogadores; j++){
+                if(document.querySelectorAll('.player')[j].classList.contains('player-ativo')){
+                    document.querySelectorAll('.player')[j].classList.remove('player-ativo');
+                }
+            }
+            document.querySelectorAll('.player')[i].classList.add('player-ativo'); 
+        
+        }
+    }
+}
+
+
+for(let i=0; i < listaNumeroDeJogadores.length; i++){
+    listaNumeroDeJogadores[i].onclick = function () {
+        numeroDeJogadores = i + 1;
+        document.querySelector('.inicio-do-jogo').classList.add('inativo');
+        console.log(numeroDeJogadores);
+    }
+    
+}
+
+
 
 for(let i=0; i<botoesPrincipais.length; i++){
     botoesPrincipais[i].onclick = function () {
@@ -17,6 +51,7 @@ for(let i=0; i<botoesPrincipais.length; i++){
 
                 imagemAberta = parseInt(imagens[i].accessKey, 10);
                 imagemAnterior = i
+
             } else if (contadorChange == 2){
                 botoesPrincipais[i].classList.add('vira');
                 imagens[i].classList.remove('oculto');
@@ -28,6 +63,11 @@ for(let i=0; i<botoesPrincipais.length; i++){
                     imagens[imagemAnterior].classList.add('imagem-ativa');
                     botoesPrincipais[imagemAnterior].classList.add('botao-ativo');
 
+                } else {
+                    console.log(contadorMultiplayer)
+                    contadorMultiplayer = (contadorMultiplayer + 1) % numeroDeJogadores;
+                    console.log(contadorMultiplayer)
+                    jogadorAtivo(contadorMultiplayer);
                 }
                 
 
@@ -42,8 +82,11 @@ for(let i=0; i<botoesPrincipais.length; i++){
                      imagens[imagemAnterior].classList.add('imagem-ativa');
                      botoesPrincipais[imagemAnterior].classList.add('botao-ativo');
 
+                } else {
+                    contadorMultiplayer = (contadorMultiplayer + 1) % numeroDeJogadores;
+                    jogadorAtivo(contadorMultiplayer);
                 }
-
+                
             } else if (contadorChange > 2 && contadorChange % 2 == 1){
                 for(let j=0; j < botoesPrincipais.length; j++){
                     if(!imagens[j].classList.contains('oculto') && !imagens[j].classList.contains('imagem-ativa')){
